eb5a49cca6295bc817b3ad29688534c672000022
daniel0514 <daniel0514@gmail.com>
2017-05-13 00:05:41 -0700

    fixed responsiveness in aboutme page. added min width and height for imgs in homepage

--- src/app/Components/aboutme/aboutme.component.ts
+++ src/app/Components/aboutme/aboutme.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-aboutme',
   templateUrl: './aboutme.component.html',
-  styleUrls: ['./aboutme.component.css', '../../../assets/css/shape.css']
+  styleUrls: ['./aboutme.component.css']
 })
 export class AboutmeComponent{
 
